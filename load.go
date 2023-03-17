19fc156ea8304ebec745dfee08a5e216d8521dfd
alexander.fridman@edzlabs.com <alexander.fridman@edzlabs.com>
2023-03-17 14:21:34 +0300

    GetKey

--- load.go
+++ load.go
@@ -3,11 +3,18 @@ package oauthutils
 import (
 	"crypto/rsa"
 	"os"
-	
+
 	"github.com/golang-jwt/jwt"
 )
 
+var verifyKey *rsa.PublicKey
+
 func LoadRSAPublicKeyFromDisk(location string) *rsa.PublicKey {
+	verifyKey = loadRSAPublicKeyFromDisk(location)
+	return verifyKey
+}
+
+func loadRSAPublicKeyFromDisk(location string) *rsa.PublicKey {
 	keyData, e := os.ReadFile(location)
 	if e != nil {
 		panic(e.Error())
@@ -17,4 +24,8 @@ func LoadRSAPublicKeyFromDisk(location string) *rsa.PublicKey {
 		panic(e.Error())
 	}
 	return key
+}
+
+func GetKey(token *jwt.Token) (interface{}, error) {
+	return verifyKey, nil
 }
